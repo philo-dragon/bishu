71895df3c274c46e1da89c395f3f077a2c579627
philo <panfeilong2012@163.com>
2018-06-11 23:41:11 +0800

    修改选择图片功能

--- module_user/src/main/java/com/pfl/module_user/utils/SelectPictureHelper.java
+++ module_user/src/main/java/com/pfl/module_user/utils/SelectPictureHelper.java
@@ -41,7 +41,7 @@ public class SelectPictureHelper {
     private int tag = -1;
 
     private Activity mActivity;
-    private OnSelectPictureListener mOnSelectPictureListener;
+    private Uri uritempFile;
 
     public SelectPictureHelper(Activity activity) {
         this.mActivity = activity;
@@ -62,8 +62,12 @@ public class SelectPictureHelper {
     public void getPicFromCamera(String fileName, int resid) {
         this.fileName = fileName;
         this.tag = resid;
+        File parentFile = new File(Environment.getExternalStorageDirectory().getPath() + "/bishu");
+        if (!parentFile.exists()) {
+            parentFile.mkdirs();
+        }
         //用于保存调用相机拍照后所生成的文件
-        tempFile = new File(Environment.getExternalStorageDirectory().getPath() + "/bishu", System.currentTimeMillis() + ".jpg");
+        tempFile = new File(parentFile, System.currentTimeMillis() + ".jpg");
         //跳转到调用系统相机
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //判断版本
@@ -102,43 +106,30 @@ public class SelectPictureHelper {
      * 裁剪图片
      */
     private void cropPhoto(Uri uri) {
-        Uri outputUri = getUri();
-        Intent intent = new Intent("com.android.camera.action.CROP");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        }
-        intent.setDataAndType(uri, "image/*");
-        intent.putExtra("crop", "true");
-        intent.putExtra("aspectX", 1);
-        intent.putExtra("aspectY", 1);
-        intent.putExtra("scale", true);
-        intent.putExtra("return-data", false);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
-        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
-        intent.putExtra("noFaceDetection", true);
-        mActivity.startActivityForResult(intent, CROP_REQUEST_CODE);
 
-       /* Intent intent = new Intent("com.android.camera.action.CROP");
+        Intent intent = new Intent("com.android.camera.action.CROP");
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        intent.setDataAndType(uri, "image*//*");
-        intent.putExtra(fileName, "true");
-        intent.putExtra("aspectX", 800);
-        intent.putExtra("aspectY", 500);
-        intent.putExtra("outputX", 360);
-        intent.putExtra("outputY", 225);
-        intent.putExtra("return-data", true);
-        mActivity.startActivityForResult(intent, CROP_REQUEST_CODE);*/
-    }
-
-    private Uri getUri() {
-        File appDir = new File(Environment.getExternalStorageDirectory().getPath() + "/bishu");
-        if (!appDir.exists()) {
-            appDir.mkdir();
+        intent.setDataAndType(uri, "image/*");
+        // crop为true是设置在开启的intent中设置显示的view可以剪裁
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 855);
+        intent.putExtra("aspectY", 510);
+        // outputX,outputY 是剪裁图片的宽高
+        intent.putExtra("outputX", 855);
+        intent.putExtra("outputY", 510);
+        //裁剪后的图片Uri路径，uritempFile为Uri类变量
+        File parentFile = new File(Environment.getExternalStorageDirectory().getPath() + "/bishu");
+        if (!parentFile.exists()) {
+            parentFile.mkdirs();
         }
-        String name = tempFile + ".jpg";
-        File file = new File(appDir, name);
-        return Uri.fromFile(file);
+        uritempFile = Uri.parse("file://" + "/" + parentFile.getAbsolutePath() + "/" + "small.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uritempFile);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        mActivity.startActivityForResult(intent, CROP_REQUEST_CODE);
+
+
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -162,85 +153,40 @@ public class SelectPictureHelper {
                 }
                 break;
             case CROP_REQUEST_CODE:     //调用剪裁后返回
-                if (intent != null) {
-                    Bundle extras = intent.getExtras();
-                    System.out.println(extras.toString());
+
+                //将Uri图片转换为Bitmap
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(mActivity.getContentResolver().openInputStream(uritempFile));
+                    //也可以进行一些保存、压缩等操作后上传
+                    String path = saveImage(fileName, bitmap);
+                    if (mOnSelectPictureSuccess != null) {
+                        mOnSelectPictureSuccess.onSelected(path, bitmap);
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
                 break;
         }
     }
 
 
-    /**
-     * 通过uri获取图片并进行压缩
-     *
-     * @param uri
-     */
-    public Bitmap getBitmapFormUri(Uri uri) throws FileNotFoundException, IOException {
-        InputStream input = mActivity.getContentResolver().openInputStream(uri);
-        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
-        onlyBoundsOptions.inJustDecodeBounds = true;
-        onlyBoundsOptions.inDither = true;//optional
-        onlyBoundsOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;//optional
-        BitmapFactory.decodeStream(input, null, onlyBoundsOptions);
-        input.close();
-        int originalWidth = onlyBoundsOptions.outWidth;
-        int originalHeight = onlyBoundsOptions.outHeight;
-        if ((originalWidth == -1) || (originalHeight == -1))
-            return null;
-        //图片分辨率以480x800为标准
-        float hh = 800f;//这里设置高度为800f
-        float ww = 480f;//这里设置宽度为480f
-        //缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
-        int be = 1;//be=1表示不缩放
-        if (originalWidth > originalHeight && originalWidth > ww) {//如果宽度大的话根据宽度固定大小缩放
-            be = (int) (originalWidth / ww);
-        } else if (originalWidth < originalHeight && originalHeight > hh) {//如果高度高的话根据宽度固定大小缩放
-            be = (int) (originalHeight / hh);
+    public String saveImage(String name, Bitmap bmp) {
+        File appDir = new File(Environment.getExternalStorageDirectory().getPath() + "/bishu");
+        if (!appDir.exists()) {
+            appDir.mkdir();
         }
-        if (be <= 0)
-            be = 1;
-        //比例压缩
-        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
-        bitmapOptions.inSampleSize = be;//设置缩放比例
-        bitmapOptions.inDither = true;//optional
-        bitmapOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;//optional
-        input = mActivity.getContentResolver().openInputStream(uri);
-        Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
-        input.close();
-
-        return compressImage(bitmap);//再进行质量压缩
-    }
-
-    /**
-     * 质量压缩方法
-     *
-     * @param image
-     * @return
-     */
-    public Bitmap compressImage(Bitmap image) {
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
-        int options = 100;
-        while (baos.toByteArray().length / 1024 > 100) {  //循环判断如果压缩后图片是否大于100kb,大于继续压缩
-            baos.reset();//重置baos即清空baos
-            //第一个参数 ：图片格式 ，第二个参数： 图片质量，100为最高，0为最差  ，第三个参数：保存压缩后的数据的流
-            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
-            options -= 10;//每次都减少10
+        String fileName = name + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+            return file.getAbsolutePath();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
-        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
-        return bitmap;
-    }
-
-
-    public void setOnSelectPictureListener(OnSelectPictureListener listener) {
-        this.mOnSelectPictureListener = listener;
-    }
-
-    public interface OnSelectPictureListener {
-        void onSelectedPicetre(String path);
+        return null;
     }
 
     private OnSelectPictureSuccess mOnSelectPictureSuccess;
