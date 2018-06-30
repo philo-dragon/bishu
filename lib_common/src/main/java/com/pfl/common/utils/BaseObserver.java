bd8e3ba5769affd24c08bfd69af59927c1373ef2
philo <panfeilong2012@163.com>
2018-06-30 14:24:15 +0800

    title下面添加阴影

--- lib_common/src/main/java/com/pfl/common/utils/BaseObserver.java
+++ lib_common/src/main/java/com/pfl/common/utils/BaseObserver.java
@@ -1,5 +1,7 @@
 package com.pfl.common.utils;
 
+import android.util.Log;
+
 import com.blankj.utilcode.util.ToastUtils;
 import com.blankj.utilcode.util.Utils;
 
@@ -12,24 +14,33 @@ import io.reactivex.disposables.Disposable;
 
 public class BaseObserver<T> implements Observer<T> {
 
+    private Disposable mDisposable;
+
     @Override
     public void onSubscribe(Disposable d) {
-
+        this.mDisposable = d;
     }
 
     @Override
     public void onNext(T httpResponse) {
-
+        Log.e("BaseObserver", "onError");
     }
 
     @Override
     public void onError(Throwable e) {
         Utils.init(App.getInstance());
         ToastUtils.showShort(e.getMessage());
+        if (!mDisposable.isDisposed()) {
+            mDisposable.dispose();
+        }
+        Log.e("BaseObserver", "onError");
     }
 
     @Override
     public void onComplete() {
-
+        if (!mDisposable.isDisposed()) {
+            mDisposable.dispose();
+        }
+        Log.e("BaseObserver", "onComplete");
     }
 }
